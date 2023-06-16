b78a16e916e5273f7adfc85ebc98049182b850fa
majocast <marcjgcastro@gmail.com>
2023-06-15 19:17:31 -0700

    changes to formatting in about page

--- personal-website/src/pages/About.tsx
+++ personal-website/src/pages/About.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import '../styles/Pages.css';
 import '../styles/About.css';
 
 const Wrapper = styled.div`
@@ -7,18 +8,33 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   height: 100vh;
-  border: 2px solid white;
 `;
 
+type DivId = 'about' | 'experience' | '';
+
 function About() {
+  const [clickedDiv, setClickedDiv] = useState<DivId>('about');
+
+  useEffect(() => {
+    setClickedDiv('about');
+  }, []);
+
+  const clickHandler = (divId: DivId): void => {
+    setClickedDiv(divId);
+  };
+
   return (
     <Wrapper id='about'>
       <div className='about-container'>
-        <div className='column'>
-          <div className='info-nav'></div>
+        <div id='about-nav'>
+          <div onClick={() => clickHandler('about')}>about</div>
+          <div onClick={() => clickHandler('experience')}>experience</div>
         </div>
-        <div className='column'>
-
+        <div id='about-content'>
+          <div className='display-cont'>
+            {(clickedDiv === 'about' || clickedDiv === '') && <div id='about-cont'>About Content</div>}
+            {clickedDiv === 'experience' && <div id='experience-cont'>Experience Content</div>}
+          </div>
         </div>
       </div>
     </Wrapper>
