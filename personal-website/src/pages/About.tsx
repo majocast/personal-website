ecf14eafa87d5cf14bb2f0839e8e8c769dfd044c
Marc Joseph Guerrero Castro <marcjgcastro@gmail.com>
2023-06-19 01:44:36 -0700

    styling & transitional changes to about page

--- personal-website/src/pages/About.tsx
+++ personal-website/src/pages/About.tsx
@@ -11,24 +11,48 @@ const Wrapper = styled.div`
 `;
 
 type DivId = 'about' | 'experience' | '';
+const originalHTML: string[] = ['about', 'experience']; 
 
 function About() {
   const [clickedDiv, setClickedDiv] = useState<DivId>('about');
 
   useEffect(() => {
     setClickedDiv('about');
+    const spans = document.getElementById('about-nav');
+    if(spans) {
+      const aboutChild = spans.querySelector(':first-child');
+      if(aboutChild) {
+        aboutChild.classList.add('active');
+      }
+    }
   }, []);
 
-  const clickHandler = (divId: DivId): void => {
+  const clickHandler = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>, divId: DivId): void => {
+    const currentDiv = document.getElementById(clickedDiv + '-cont');
+    const currentSpans = document.getElementsByClassName('active');
+    for(let i = 0; i < currentSpans.length; i++) {
+      const element = currentSpans[i] as HTMLElement;
+      element.classList.remove('active');
+    }
+    console.log(currentDiv);
+    if(clickedDiv !== divId) {
+      currentDiv?.classList.remove('display');
+    }
     setClickedDiv(divId);
+    document.getElementById(clickedDiv + '-cont')?.classList.add('display');
+    console.log(document.getElementById(clickedDiv + '-cont'));
+    console.log(document.getElementById(clickedDiv + '-cont')?.classList);
+
+    const target = event.target as HTMLElement;
+    target.classList.add('active');
   };
 
   return (
     <Wrapper id='about'>
       <div className='about-container'>
         <div id='about-nav'>
-          <div onClick={() => clickHandler('about')}>about</div>
-          <div onClick={() => clickHandler('experience')}>experience</div>
+          <span onClick={(e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => clickHandler(e, 'about')}>about</span>
+          <span onClick={(e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => clickHandler(e, 'experience')}>experience</span>
         </div>
         <div id='about-content'>
           <div className='display-cont'>
